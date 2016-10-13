b85ef6e59bd18775d8461aabc512c8764e210759
sahanasadagopan <sahana.sadagopan@gmail.com>
2016-10-12 21:37:38 -0600

    Add files via upload

---
+++ project2/Basic_C%2520file/uart.h
@@ -0,0 +1,12 @@
+/*
+ * uart.h
+ *
+ *  Created on: Oct 11, 2016
+ *      Author: Divya Sampath
+ */
+
+#ifndef SOURCES_UART_H_
+#define SOURCES_UART_H_
+void uart0_init (int sysclk, int baud);
+//void uart_write_byte(char c);
+#endif /* SOURCES_UART_H_ */
